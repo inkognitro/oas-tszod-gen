cbf41ac773e1c19b798e85517cfb1c17e2b90f63
Maece Fischer <contact@maecefischer.ch>
2024-03-17 17:52:49 +0100

    fix reading file contents

--- src/generate.ts
+++ src/generate.ts
@@ -4,25 +4,21 @@ const fs = require('fs');
 
 export function generate() {
   const filePath = './oasTestSpecs.json';
-  fs.readFile(filePath, 'utf-8', (error: any, data: object) => {
-    if (error) {
-      console.error(`could not load file from filePath "${filePath}"`, error);
-      return;
-    }
-    if (!isSpecification(data)) {
-      console.error('specs are invalid...');
-      return;
-    }
-    console.info('specs are valid :)');
-    /*if (!isSpecification())
-      specs.generateFiles({
-        createFile: (filePath, content) => {
-          console.log(`filePath: ${filePath}`);
-          console.log(content);
-        },
-      });
-      */
-  });
+  const dataStr = fs.readFileSync(filePath, 'utf-8').toString();
+  const data = JSON.parse(dataStr);
+  if (!isSpecification(data)) {
+    console.error('specs are invalid :(');
+    return;
+  }
+  console.info('specs are valid :)');
+  /*if (!isSpecification())
+    specs.generateFiles({
+      createFile: (filePath, content) => {
+        console.log(`filePath: ${filePath}`);
+        console.log(content);
+      },
+    });
+  */
 }
 
 generate();
