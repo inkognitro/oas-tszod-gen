c34afc478c253aa2e6d1703df75eaa1b87996ce5
Maece Fischer <contact@maecefischer.ch>
2024-09-09 22:28:19 +0200

    request cookies for FetchApiRequestHandler

--- src/templates/ts/core/fetchApiRequestHandler.ts
+++ src/templates/ts/core/fetchApiRequestHandler.ts
@@ -1,34 +1,47 @@
-import {Cookies, Request, RequestHandler, RequestResult} from './core';
+import {
+  Headers,
+  Request,
+  RequestHandler,
+  RequestResult,
+  Response as CoreResponse,
+  ResponseSetCookies,
+} from './core';
 import {stringify} from 'qs';
 
-type CreateWithCookiesEnrichedRequestInitFunc = (
-  currentRequestInit: RequestInit,
-  cookies: Cookies
-) => RequestInit;
+class ResultResponse implements CoreResponse {
+  private readonly response: Response;
+  private readonly cachedBody: undefined | any;
+
+  public readonly statusCode: number;
+  public readonly headers: Headers;
+  public readonly cookies: ResponseSetCookies;
+
+  constructor(response: Response) {
+    this.response = response;
+    this.statusCode = response.status;
+    this.headers = this.createPlainHeaders(response);
+    this.cookies = this.createPlainCookies(response);
+  }
+
+  private createPlainHeaders(response: Response): Headers {
+    return {}; // todo: implement
+  }
+
+  private createPlainCookies(response: Response): ResponseSetCookies {
+    return {}; // todo: implement
+  }
+
+  revealBody(): Promise<any> {
+    // todo: implement
+    return new Promise(resolve => {
+      resolve('foo');
+    });
+  }
+}
 
 type FetchApiRequestHandlerConfig = {
-  generalRequestInit: RequestInit;
-  createWithCookiesEnrichedRequestInit: CreateWithCookiesEnrichedRequestInitFunc;
-};
-
-export const defaultFetchApiRequestHandlerRequestInit: RequestInit = {
-  credentials: 'same-origin',
-};
-
-const defaultConfig: FetchApiRequestHandlerConfig = {
-  generalRequestInit: defaultFetchApiRequestHandlerRequestInit,
-  createWithCookiesEnrichedRequestInit: currentRequestInit => {
-    const warningParts = [
-      'The default configuration of the FetchApiRequestHandler class does not implement cookie value enrichment',
-      "for the RequestInit-parameter of the Javascript's built-in Fetch API.",
-      'Please specify your own "createWithCookiesEnrichedRequestInit" function in your custom configuration',
-      'when calling the FetchApiRequestHandler\'s constructor with "new FetchApiRequestHandler(customConfig)".',
-      'Please be aware that servers and clients should define different headers in your',
-      '"createWithCookiesEnrichedRequestInit" function: "Set-Cookie" (server) Vs "Cookie" (client).',
-    ];
-    console.warn(warningParts.join(' '));
-    return currentRequestInit;
-  },
+  baseUrl?: string;
+  generalRequestInit?: RequestInit;
 };
 
 type AbortControllerByRequestId = {
@@ -36,92 +49,147 @@ type AbortControllerByRequestId = {
 };
 
 export type FetchApiRequestHandlerExecuteConfig = {
-  fetchApiRequestInit?: RequestInit;
-  ignoreGeneralFetchApiRequestInit?: boolean;
+  refineFetchApiRequestInit?: (preparedRequestInit: RequestInit) => RequestInit;
 };
 
 export class FetchApiRequestHandler implements RequestHandler {
   private readonly config: FetchApiRequestHandlerConfig;
+  private readonly generalRequestInit: undefined | RequestInit;
   private readonly abortControllerByPendingRequestIds: AbortControllerByRequestId;
 
-  constructor(config: FetchApiRequestHandlerConfig = defaultConfig) {
+  constructor(
+    config: FetchApiRequestHandlerConfig,
+    generalRequestInit?: RequestInit
+  ) {
     this.config = config;
+    this.generalRequestInit = generalRequestInit;
     this.abortControllerByPendingRequestIds = {};
+    this.execute = this.execute.bind(this);
+    this.cancelRequestById = this.cancelRequestById.bind(this);
+    this.cancelAllRequests = this.cancelAllRequests.bind(this);
+    this.createRequestInit = this.createRequestInit.bind(this);
   }
 
   public execute(
     request: Request,
     config?: FetchApiRequestHandlerExecuteConfig
   ): Promise<RequestResult> {
-    return new Promise(resolve => {
-      const queryStrUrlPart = Object.keys(request.queryParams).length
-        ? `?${stringify(request.queryParams)}`
-        : null;
+    const abortControllerByPendingRequestIds =
+      this.abortControllerByPendingRequestIds;
+    return new Promise((resolve, reject) => {
+      const queryStrUrlPart =
+        request.queryParams && Object.keys(request.queryParams).length
+          ? `?${stringify(request.queryParams)}`
+          : null;
       const abortController = new AbortController();
-      this.abortControllerByPendingRequestIds[request.id] = abortController;
+      abortControllerByPendingRequestIds[request.id] = abortController;
       const requestInit: RequestInit = {
-        ...this.createFetchApiRequestInit(request, config),
+        ...this.createRequestInit(request, config),
         signal: abortController.signal,
       };
-      fetch(`${request.url}${queryStrUrlPart}`, requestInit)
+      fetch(
+        `${this.config.baseUrl}${request.url}${queryStrUrlPart}`,
+        requestInit
+      )
         .then(response => {
-          if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-          }
-          // const json = await response.json().then(); // todo: resolve and decide whether response should have GetBody() method instead
-          // console.log(json);
+          resolve({
+            request: request,
+            response: new ResultResponse(response),
+            hasRequestBeenCancelled: abortController.signal.aborted,
+          });
         })
         .catch(error => {
-          console.error(error.message);
+          if (abortController.signal.aborted) {
+            resolve({
+              request: request,
+              response: error.response
+                ? new ResultResponse(error.response)
+                : null,
+              hasRequestBeenCancelled: true,
+            });
+            return;
+          }
+          resolve({
+            request: request,
+            response: error.response
+              ? new ResultResponse(error.response)
+              : null,
+            hasRequestBeenCancelled: false,
+            error,
+          });
+        })
+        .finally(() => {
+          delete abortControllerByPendingRequestIds[request.id];
         });
     });
   }
 
   cancelRequestById(requestId: string) {
     const controller = this.abortControllerByPendingRequestIds[requestId];
-    if (!controller) {
-      return;
+    if (controller) {
+      controller.abort();
     }
-    controller.abort();
-    delete this.abortControllerByPendingRequestIds[requestId];
   }
 
   cancelAllRequests() {
     for (const requestId in this.abortControllerByPendingRequestIds) {
       const controller = this.abortControllerByPendingRequestIds[requestId];
-      if (!controller) {
-        return;
+      if (controller) {
+        controller.abort();
       }
-      controller.abort();
-      delete this.abortControllerByPendingRequestIds[requestId];
     }
   }
 
-  private createFetchApiRequestInit(
+  private createRequestInit(
     request: Request,
     config?: FetchApiRequestHandlerExecuteConfig
   ): RequestInit {
-    let requestInit: RequestInit = {
-      ...(config?.ignoreGeneralFetchApiRequestInit
-        ? {}
-        : this.config.generalRequestInit),
+    const requestInit: RequestInit = {
+      ...(this.generalRequestInit ? this.generalRequestInit : {}),
       method: request.endpointId.method,
     };
     if (request.headers) {
-      requestInit.headers = request.headers;
+      requestInit.headers = this.createRequestInitHeaders(requestInit, request);
     }
     if (request.body) {
       requestInit.body = request.body;
     }
+    if (!config?.refineFetchApiRequestInit) {
+      return requestInit;
+    }
+    return config.refineFetchApiRequestInit(requestInit);
+  }
+
+  private createRequestInitHeaders(
+    requestConfig: RequestInit,
+    request: Request
+  ): HeadersInit {
+    const cookieHeaders = request.cookies
+      ? this.findRequestInitCookieHeaders(request)
+      : {};
+    return {
+      ...(requestConfig.headers ?? {}),
+      ...(request.headers ?? {}),
+      ...cookieHeaders,
+    };
+  }
+
+  private findRequestInitCookieHeaders(request: Request): null | HeadersInit {
+    const currentCookieDefinition = request.headers?.['Cookie'];
+    const cookieDefinitions: string[] = currentCookieDefinition
+      ? [currentCookieDefinition]
+      : [];
     if (request.cookies) {
-      requestInit = this.config.createWithCookiesEnrichedRequestInit(
-        requestInit,
-        request.cookies
-      );
+      for (const cookieName in request.cookies) {
+        const cookieValue = request.cookies[cookieName];
+        cookieDefinitions.push(`${cookieName}=${cookieValue}`);
+      }
+    }
+    if (!cookieDefinitions.length) {
+      return null;
     }
     return {
-      ...requestInit,
-      ...(config?.fetchApiRequestInit ?? {}),
+      Cookie: cookieDefinitions.join('; '),
     };
   }
 }
