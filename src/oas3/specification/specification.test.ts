06907161f476ec139fd42019bf5e207d3008751c
Maece Fischer <contact@maecefischer.ch>
2024-09-24 18:05:03 +0200

    Update specification.test.ts

--- src/oas3/specification/specification.test.ts
+++ src/oas3/specification/specification.test.ts
@@ -1,13 +1,18 @@
 import {isSpecification} from './specification';
 const petStore1Specs = require('../../../example-specs/petstore1.oas3.json');
 const petStore2Specs = require('../../../example-specs/petstore2.oas3.json');
+const binanceSpecs = require('../../../example-specs/binance.oas3.json');
 
-test('isSpecification validates pet store one specification as correct input', () => {
+test('isSpecification validates pet store 1 specification as correct input', () => {
   expect(isSpecification(petStore1Specs)).toBe(true);
 });
 
-test('isSpecification validates pet store two specification as correct input', () => {
+test('isSpecification validates pet store 2 specification as correct input', () => {
   expect(isSpecification(petStore2Specs)).toBe(true);
+});
+
+test('isSpecification validates binance specification as correct input', () => {
+  expect(isSpecification(binanceSpecs)).toBe(true);
 });
 
 test('isSpecification invalidates incorrect input', () => {
