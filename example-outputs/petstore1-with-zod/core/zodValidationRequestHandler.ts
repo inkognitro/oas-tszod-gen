3311f3c9b693a398d1ee7117ca8382f8d7f9fd11
Maece Fischer <contact@maecefischer.ch>
2024-09-25 17:59:02 +0200

    refresh code generation output

--- example-outputs/petstore1-with-zod/core/zodValidationRequestHandler.ts
+++ example-outputs/petstore1-with-zod/core/zodValidationRequestHandler.ts
@@ -1,4 +1,5 @@
 import {Request, RequestHandler, RequestResult, ResponseSchema} from './core';
+import {z, ZodSchema} from 'zod';
 
 export type ZodValidationRequestHandlerExecutionConfig = {};
 
@@ -12,76 +13,84 @@ export class ZodValidationRequestHandler implements RequestHandler {
     this.cancelAllRequests = this.cancelAllRequests.bind(this);
   }
 
-  private validateRequest(request: Request) {
-    const schema = request.endpointSchema;
-    if (schema.pathParamsZodSchema) {
-      schema.pathParamsZodSchema.parse(request.pathParams);
-    }
-    if (schema.headersZodSchema) {
-      schema.headersZodSchema.parse(request.headers);
-    }
-    if (schema.queryParamsZodSchema) {
-      schema.queryParamsZodSchema.parse(request.queryParams);
-    }
-    if (schema.cookiesZodSchema && request.cookies) {
-      schema.cookiesZodSchema.parse(request.cookies);
-    }
-    const contentType = request.contentType;
-    if (!contentType) {
-      return;
-    }
-    const bodySchema = schema.bodyByContentType[contentType];
-    if (!bodySchema || request.body instanceof FormData) {
-      return;
-    }
-    bodySchema.zodSchema.parse(request.body);
-  }
-
-  private validateResponse(rr: RequestResult) {
-    const response = rr.response;
-    if (!response) {
-      return;
-    }
-    const schema: undefined | ResponseSchema =
-      rr.request.endpointSchema.responseByStatus?.[response.status];
-    if (!schema) {
-      return;
-    }
-    if (schema.headersZodSchema) {
-      schema.headersZodSchema.parse(response.headers);
-    }
-    const contentType = response.contentType;
-    if (!contentType) {
-      return;
-    }
-    const bodySchema = schema.bodyByContentType[contentType];
-    if (!bodySchema || response.body instanceof FormData) {
-      return;
-    }
-    bodySchema.zodSchema.parse(response.body);
-  }
-
   public execute(
     request: Request,
     config?: ZodValidationRequestHandlerExecutionConfig
   ): Promise<RequestResult> {
     return new Promise((resolve, reject) => {
-      try {
-        this.validateRequest(request);
-        this.nextRequestHandler.execute(request, config).then(rr => {
-          try {
-            this.validateResponse(rr);
-            resolve(rr);
-          } catch (e) {
-            reject(e);
-            return;
-          }
-        });
-      } catch (e) {
-        reject(e);
+      const requestZodSchema = this.createRequestZodSchema(request);
+      const requestValidationResult = requestZodSchema.safeParse(request);
+      if (requestValidationResult.success) {
+        reject(requestValidationResult.error);
         return;
       }
+      this.nextRequestHandler.execute(request, config).then(rr => {
+        const requestZodSchema = this.createResponseZodSchema(rr);
+        const responseValidationResult = requestZodSchema.safeParse(
+          rr.response
+        );
+        if (!responseValidationResult.success) {
+          reject(responseValidationResult.error);
+          return;
+        }
+        resolve(rr);
+      });
     });
+  }
+
+  private createRequestZodSchema(request: Request): ZodSchema {
+    const schemaProps: Record<string, ZodSchema> = {};
+    const schema = request.endpointSchema;
+    if (schema.pathParamsZodSchema) {
+      schemaProps['pathParams'] = schema.pathParamsZodSchema;
+    }
+    if (schema.headersZodSchema) {
+      schemaProps['headers'] = schema.headersZodSchema;
+    }
+    if (schema.queryParamsZodSchema) {
+      schemaProps['queryParams'] = schema.queryParamsZodSchema;
+    }
+    if (schema.cookiesZodSchema && request.cookies) {
+      schemaProps['cookies'] = schema.cookiesZodSchema;
+    }
+    const contentType = request.contentType;
+    if (!contentType) {
+      return z.object(schemaProps);
+    }
+    const bodySchema = schema.bodyByContentType[contentType];
+    if (bodySchema && !(request.body instanceof FormData)) {
+      schemaProps['body'] = bodySchema.zodSchema;
+    }
+    return z.object(schemaProps);
+  }
+
+  private createResponseZodSchema(rr: RequestResult): ZodSchema {
+    const response = rr.response;
+    if (!response) {
+      return z.object({});
+    }
+    const schemaByStatus = rr.request.endpointSchema.responseByStatus;
+    if (!schemaByStatus) {
+      return z.object({});
+    }
+    const schema: undefined | ResponseSchema =
+      schemaByStatus[response.status] ?? schemaByStatus['default'];
+    if (!schema) {
+      return z.object({});
+    }
+    const schemaProps: Record<string, ZodSchema> = {};
+    if (schema.headersZodSchema) {
+      schemaProps['headers'] = schema.headersZodSchema;
+    }
+    const contentType = response.contentType;
+    if (!contentType) {
+      return z.object(schemaProps);
+    }
+    const bodySchema = schema.bodyByContentType[contentType];
+    if (bodySchema && !(response.body instanceof FormData)) {
+      schemaProps['body'] = bodySchema.zodSchema;
+    }
+    return z.object(schemaProps);
   }
 
   public cancelAllRequests() {
