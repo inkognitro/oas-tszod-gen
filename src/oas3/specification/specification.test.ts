937f468b1738492433081fee4eece4c013c28a6f
Maece Fischer <contact@maecefischer.ch>
2024-10-09 12:15:45 +0200

    Update specification.test.ts

--- src/oas3/specification/specification.test.ts
+++ src/oas3/specification/specification.test.ts
@@ -3,18 +3,18 @@ const petStore1Specs = require('../../../example-specs/petstore1.oas3.json');
 const petStore2Specs = require('../../../example-specs/petstore2.oas3.json');
 const binanceSpecs = require('../../../example-specs/binance.oas3.json');
 
-test('zSpecification.parse throws error with incorrect input', () => {
+test('throws error with incorrect input', () => {
   expect(() => zSpecification.parse('foo')).toThrow();
 });
 
-test('isSpecification validates PetStore1 specification as correct input', () => {
+test('parses PetStore1 specification as correct input', () => {
   expect(() => zSpecification.parse(petStore1Specs)).not.toThrow();
 });
 
-test('isSpecification validates PetStore2 specification as correct input', () => {
+test('parses PetStore2 specification as correct input', () => {
   expect(() => zSpecification.parse(petStore2Specs)).not.toThrow();
 });
 
-test('isSpecification validates Binance specification as correct input', () => {
+test('parses Binance specification as correct input', () => {
   expect(() => zSpecification.parse(binanceSpecs)).not.toThrow();
 });
