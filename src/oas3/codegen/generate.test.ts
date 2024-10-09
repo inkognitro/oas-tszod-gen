48df5112fcc454f4d5e12fd2a33a699376629d21
Maece Fischer <contact@maecefischer.ch>
2024-10-09 16:31:15 +0200

    Update generate.test.ts

--- src/oas3/codegen/generate.test.ts
+++ src/oas3/codegen/generate.test.ts
@@ -1,4 +1,4 @@
-import {generateOas3ToTs} from './generate';
+import {generateOas3ToTs, Oas3ToTsConfig} from './generate';
 const petStore1Specs = require('../../../example-specs/petstore1.oas3.json');
 const petStore2Specs = require('../../../example-specs/petstore2.oas3.json');
 const binanceSpecs = require('../../../example-specs/binance.oas3.json');
@@ -14,10 +14,10 @@ describe('Generator', () => {
           });
         },
         outputFolderPath: './example-outputs/petstore1',
+        importRootAlias: '@example-outputs/petstore1',
         logger: {
           log: () => {},
         },
-        importRootAlias: '@example-outputs/petstore1',
       });
     }).not.toThrow();
   });
@@ -31,11 +31,11 @@ describe('Generator', () => {
           });
         },
         outputFolderPath: './example-outputs/petstore1-with-zod',
+        importRootAlias: '@example-outputs/petstore1-with-zod',
         logger: {
           log: () => {},
         },
         withZod: true,
-        importRootAlias: '@example-outputs/petstore1-with-zod',
       });
     }).not.toThrow();
   });
@@ -49,10 +49,10 @@ describe('Generator', () => {
           });
         },
         outputFolderPath: './example-outputs/petstore2',
+        importRootAlias: '@example-outputs/petstore2',
         logger: {
           log: () => {},
         },
-        importRootAlias: '@example-outputs/petstore2',
       });
     }).not.toThrow();
   });
@@ -66,11 +66,11 @@ describe('Generator', () => {
           });
         },
         outputFolderPath: './example-outputs/petstore2-with-zod',
+        importRootAlias: '@example-outputs/petstore2-with-zod',
         logger: {
           log: () => {},
         },
         withZod: true,
-        importRootAlias: '@example-outputs/petstore2-with-zod',
       });
     }).not.toThrow();
   });
@@ -84,10 +84,10 @@ describe('Generator', () => {
           });
         },
         outputFolderPath: './example-outputs/binance',
+        importRootAlias: '@example-outputs/binance',
         logger: {
           log: () => {},
         },
-        importRootAlias: '@example-outputs/binance',
       });
     }).not.toThrow();
   });
@@ -101,46 +101,40 @@ describe('Generator', () => {
           });
         },
         outputFolderPath: './example-outputs/binance-with-zod',
+        importRootAlias: '@example-outputs/binance-with-zod',
         logger: {
           log: () => {},
         },
         withZod: true,
-        importRootAlias: '@example-outputs/binance-with-zod',
       });
     }).not.toThrow();
   });
+
+  const stripeConfig: Oas3ToTsConfig = {
+    getSpecification: () => {
+      return new Promise(resolve => {
+        resolve(stripeSpecs);
+      });
+    },
+    outputFolderPath: './example-outputs/stripe',
+    importRootAlias: '@example-outputs/stripe',
+    logger: {
+      log: () => {},
+    },
+  };
 
   it('can generate files from Stripe specs', () => {
     expect(() => {
-      generateOas3ToTs({
-        getSpecification: () => {
-          return new Promise(resolve => {
-            resolve(stripeSpecs);
-          });
-        },
-        outputFolderPath: './example-outputs/stripe',
-        logger: {
-          log: () => {},
-        },
-        importRootAlias: '@example-outputs/stripe',
-      });
+      generateOas3ToTs(stripeConfig);
     }).not.toThrow();
   });
 
-  // todo: reimplement (add @ts-ignore for zodSchemas having a lazy loaded schema inside)
   /*
   it('can generate files from Stripe specs with Zod', () => {
     expect(() => {
       generateOas3ToTs({
-        getSpecification: () => {
-          return new Promise(resolve => {
-            resolve(stripeSpecs);
-          });
-        },
+        ...stripeConfig,
         outputFolderPath: './example-outputs/stripe-with-zod',
-        logger: {
-          log: () => {},
-        },
         withZod: true,
         importRootAlias: '@example-outputs/stripe-with-zod',
       });
